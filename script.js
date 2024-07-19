36508d3ebaa27e99e1057e6006eeac3e542fc8b3
Sumit918 <131543199+Sumit918@users.noreply.github.com>
2024-07-19 10:59:13 +0545

    portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function togglemenu(){
+function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open")
