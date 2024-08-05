228a57adb557b40f3ba69301d540d0af5bd86c59
Sumit918 <131543199+Sumit918@users.noreply.github.com>
2024-08-05 22:50:52 +0545

    change

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const downloadLink = document.getElementById('downloadCV');
+    
+    downloadLink.addEventListener('click', function (event) {
+        console.log('CV download link clicked');
+    });
+});
